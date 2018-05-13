e5ab261d837255c719f6e24d2b78cbe30765a91c
David L <lapadula.david@gmail.com>
2018-05-13 01:30:00 -0400

    final touches

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,21 +7,21 @@ var gameQuestions = [{
 	answerList: ["Energy", "Service", "Government", "Manufacturing"],
     answer: 1, 
     image: "assets/images/service_question.jpg", 
-    message: "Statistically Speaking, most Canadians work in the service industry", 
+    message: "Statistically speaking, most Canadians work in the service industry", 
 },{ 
 	question: "What is Canada's highest mountain?",
 	answerList: ["Mount Saint Elias", "Mount Slaggard", "Mount Lucania", "Mount Logan"],
     answer: 3, 
     image: "assets/images/mount_logan.jpg", 
-    message: "At 19,551 ft, Mount Logan is Canada's tallest Mountain",  
+    message: "At 19,551 ft, Mount Logan is Canada's tallest mountain",  
 },{
-	question: "Which of these is a Canadian territory?",
+	question: "Which of the following options is a Canadian territory?",
 	answerList: ["New Brunswick", "Nunavut", "Alaska", "North Pole"],
-    answer: 1, 
+    answer: 1,  
     image: "assets/images/territories_region.png", 
     message: "Along with Yukon and the North West Territories, Nunavut is a Canadian territory", 
 },{
-	question: "Since 2000, how Many Olympic Gold Medals does the Mens Canadian Hockey team have?",
+	question: "Since 2000, how Many Olympic Gold medals does the mens Canadian hockey team have?",
 	answerList: ["3", "5", "2", "0"],
     answer: 0, 
     image: "assets/images/canada_hockey.gif", 
@@ -32,7 +32,7 @@ var gameQuestions = [{
     answer: 2, 
     image: "assets/images/parliament.jpg", 
     message: "The House of Commons is one governing body of the Canadian Parliament", 
-},{
+},{ 
 	question: "What was the last province to join Canada?",
 	answerList: ["Newfoundland", "Quebec", "The North Pole", "Alberta"],
     answer: 0,
@@ -53,34 +53,36 @@ var gameQuestions = [{
 },{
 	question: "Which day of the year is Canada Day?",
 	answerList: ["July 1st", "July 4th", "December 31st", "April 1st"],
-    answer: 0,
+    answer: 0,  
     image: "assets/images/canada_day.webp", 
-    message: "In celebration of the Constitution Act, July 1st is Canada day", 
+    message: "Canada day is July 1st, and is held in celebration of the Constitution Act", 
 },{   
 	question: "Hockey is Canada's official sport", 
 	answerList: ["True", "False"],
-    answer: 1,
+    answer: 1,  
     image: "assets/images/lacrosse.jpg", 
-    message: "Contrary to popular opinion, Lacrosse is Canada's national sport",  
+    message: "Contrary to popular opinion, Lacrosse is Canada's National sport",  
 },{
 	question: "In what year was the Canadian Flag first shown?",
 	answerList: ["1867", "1812", "1944", "1965"],
-    answer: 3,
+    answer: 3, 
     image: "assets/images/waving_flag.gif", 
     message: "Designed by George Stanley, the official Canadian flag was established in 1965",  
 },{
 	question: "The Toronto Maple Leafs won a Stanley Cup in the last 50 years",
 	answerList: ["True", "False"], 
-    answer: 1, 
+    answer: 1,  
     image: "assets/images/1967_cup.jpg", 
     message: "With their last win coming in 1967, they have not captured Lord Stanley in over a half century",    
 }]; 
+  
+//After each question is generated its index gets pushed into this array. This array is then used as a check to ensure no question is used twice in one game
+var indexOfUsed = [];    
             
 //Selectors for in game play
 var gameHeading = $("#game-heading"); 
 var gameStartDiv = $("#game-start"); 
 var startButton = $("#start-button"); 
-var restartButton = $("#play-again"); 
 var gamePlayDiv = $("#game-play"); 
 var questionDiv = $("#question-div"); 
 var optionDiv = $("#answer-options"); 
@@ -110,6 +112,7 @@ var currentAnswerImage;
 var currentAnswerMessage;
 var timerSeconds; 
 var timedInterval; 
+var buttonStyle = "col-md-12 btn btn-secondary btn-lg mt-2 h2 font-weight-bold text-light answer-click"; 
 
 // Counters used to calculate end of game statistics
 var questionsUsed = 0; 
@@ -119,18 +122,14 @@ var timeoutAnswers = 0;
 var timeUsed = 0;
 var percentScore = 0; 
 var secondsRemaining = 0;
-var buttonStyle = "col-md-12 btn btn-secondary btn-lg mt-2 h2 font-weight-bold text-light answer-click"; 
   
-// Conditions for the beggining of the game (function)  *******
-var gameStartConditions = function () {}
-    gamePlayDiv.hide(); 
-    progressSection.hide();  
-    gameResultsDiv.hide(); 
-    gameStartDiv.show(); 
+// Conditions for the beggining of the game when the page loads
+gamePlayDiv.hide(); 
+progressSection.hide();  
+gameResultsDiv.hide(); 
+gameStartDiv.show();  
+startButton.text('GO FOR TRIVIA')
 
-// Conditions for the restarting the game (function)  *******
-var newGame = function () {}
-  
 //function for generating the question and placing it on the screen  
 var questionGenerator = function () {
    questionDiv.empty();  //hides elements that are not part of gamplay and empties div that showed previous question when a new one is generated
@@ -138,8 +137,10 @@ var questionGenerator = function () {
    gameResultsDiv.hide()
    gamePlayDiv.show();   
    gameHeading.text('Question')
-   questionsUsed++;    
-   questionObject = gameQuestions[Math.floor(Math.random()* gameQuestions.length)]; //gets random object from questions array and sets values in variables *****
+   unusedWord(); 
+   indexOfUsed.push(gameQuestions.indexOf(questionObject));  
+   console.log(indexOfUsed); 
+   questionsUsed++;       
    currentQuestion = questionObject.question; 
    currentAnswerList = questionObject.answerList; 
    currentAnswer = questionObject.answer;  
@@ -152,10 +153,10 @@ var questionGenerator = function () {
         answerButton.text((i+1) + ") " + currentAnswerList[i]);
         answerButton.attr({'data-index': i})
         optionDiv.append(answerButton);  
-    }  
+    }    
     timeClock();  
 
-    // collects the data of the user click and stops the timer 
+// collects the data of the user click and stops the timer 
     $('.answer-click').on('click', function() { 
         gameSound.attr('src', 'assets/audio/button_click.mp3')  
         userResponded = true; 
@@ -163,12 +164,12 @@ var questionGenerator = function () {
         clearInterval(timedInterval); 
         userResults(); 
     }) 
- 
+  
 }      
-   
-// Two functions for counting down once a new question is clicked.
+     
+// Two functions for counting down. The counting interval is cleared if the timer runs out or the user makes a choice
  var timeClock = function () {
-     timerSeconds = 20; 
+     timerSeconds = 5; 
      timer.text('Remaining Time: ' + timerSeconds)
      timedInterval = setInterval(timeCountdown, 1000) 
  } 
@@ -180,94 +181,125 @@ var questionGenerator = function () {
          userResults()
      } 
    
- }  
+ }   
 
 //sets conditions for the result of the reponse and then runs the game status function in 5 seconds
  var userResults = function () { 
         gamePlayDiv.hide();
         finalStatsTable.hide();   
         gameResultsDiv.show(); 
-        gameHeading.text('Result') 
+        gameHeading.text('Result'); 
         imageUpdate.attr('src', currentAnswerImage)
         finalResultDiv.text(currentAnswerMessage);
         timeUsed = timeUsed += timerSeconds; 
-        if ((userChoice === currentAnswer) && (userResponded === true)) { //user responded boolean to distinguish when user has made a choice vs. non-answer
+        if ((userResponded === true) && (userChoice === currentAnswer)) { //user responded boolean to distinguish when user has made a choice vs. non-answer
             correctAnswers++;  
             answerResultDiv.text('That is Correct!') 
-        } else if ((userChoice != currentAnswer) && (userResponded === true)) {
+        } else if ((userResponded === true) && (userChoice != currentAnswer)) {
             wrongAnswers++; 
             answerResultDiv.text('That is Incorrect!')
         } else { 
             timeoutAnswers++
             answerResultDiv.text('Out of Time!') 
         } 
-        userResponded = false;   
+        userResponded = false;    
         setTimeout(gameStatus, 5000);          
  }  
 
- // Checks if all the questions have been answered first. If so, shows displays calculating bar and then runs game over in 5 seconds. Otherwise generates new question
+//Checks how many questions have been asked and sets the game accordingly
  var gameStatus = function () {   
-     if (questionsUsed === 1) { //questionsAnswered.length 
-        calculatingResult(); 
-        setTimeout(gameOver, 5000);   
-     } else {
-        questionGenerator(); 
-     }
+    if (questionsUsed === gameQuestions.length) { 
+       calculatingResult(); 
+       setTimeout(gameOver, 5000);   
+    } else {
+       questionGenerator(); 
+    }
 
- }         
-  
+}  
+
+ // Returns an object from gameQuestions array and ensures it has not been used already
+ var unusedWord = function () {   
+    questionObject = gameQuestions[Math.floor(Math.random()* gameQuestions.length)];
+    console.log(questionObject, '1')
+        if (indexOfUsed.length > 0) { 
+            console.log(questionObject, '2')
+            for (var i = 0; i < indexOfUsed.length + 1; i++) { /// try a for each loop here or less than or equal to
+                if (indexOfUsed.includes(gameQuestions.indexOf(questionObject))) { 
+                    questionObject = gameQuestions[Math.floor(Math.random()* gameQuestions.length)];
+                    console.log( '3') 
+                    } else { 
+                        console.log('4')
+                        return questionObject; 
+                    }
+            } 
+        }   
+ }       
+ 
+    
 // Displays the progress bar for 5 seconds before the results are calculated and displayed
  var calculatingResult = function () {
     progressSection.show(); 
     answerResultDiv.text(''); 
-    finalResultDiv.text('Calculating');
+    finalResultDiv.text('Gimme a second, Eh...');
     imageUpdate.hide(); 
-    gameHeading.text('Please await your score')
-        var progressBar = document.getElementById("progress-bar")   // emebedded function to update progress bar ****
-        var width = 1; 
-        var id = setInterval(frame, 10);
-        function frame() {  
-            if (width >= 100) {
+    gameHeading.text('Calculating');    
+        var progressBar = document.getElementById("progress-bar")   // nested function to update progress bar 
+        var width = 0; 
+        var id = setInterval(incrementBar, 10);
+        function incrementBar() {  
+            if (width >= 100) { 
                 clearInterval(id); 
             } else {
-                width = width + 0.2;   
-                progressBar.style.width = width + '%'; 
+                width = width + 0.205;   
+                progressBar.style.width = width + '%';  
             }
         }
- }  
+ }     
        
+ //function is run when after all the questions have been answered. It shows the final screen, calculates values and determines is the game was a win or a loss
  var gameOver = function () {
     percentScore = ((correctAnswers/gameQuestions.length) * 100).toFixed();   
     progressSection.hide();  
-    gameHeading.text('Game Over')
-    finalMessageDiv.text('Analytics'); 
+    gameHeading.text('Game Over');
+    finalMessageDiv.show().text('Analytics');  
     finalStatsTable.show();  
-    correctAmount.append('<th>' + correctAnswers + '</th>')
-    incorrectAmount.append('<th>' + wrongAnswers + '</th>')
+    startButton.show().text('ANOTHER ROUND?')
+    correctAmount.append('<th>' + correctAnswers + '</th>');
+    incorrectAmount.append('<th>' + wrongAnswers + '</th>');
     avgTime.append('<th>' + (timeUsed/gameQuestions.length).toFixed(1) + ' seconds </th>')
-    percentCorrect.append('<th>' + percentScore + ' % </th>')
-    timeOutAmount.append('<th>' + timeoutAnswers + ' </th>')
-    if (percentScore >= 1) { 
+    percentCorrect.append('<th>' + percentScore + ' % </th>');
+    timeOutAmount.append('<th>' + timeoutAnswers + ' </th>');
+    if (percentScore >= 70) {  
         answerResultDiv.text('You have won the game !');
-        finalResultDiv.text('You got ' + correctAnswers + ' correct ' + ' out of ' + gameQuestions.length);
+        finalResultDiv.text(`You got ${correctAnswers} correct out of ${gameQuestions.length}! Brilliant !`);
         imageUpdate.show().attr('src', 'assets/images/win.webp');
-        gameSound.attr('src', 'assets/audio/national_anthem.mp3')  
-    } else if (percentScore < 80) {
+        gameSound.attr('src', 'assets/audio/national_anthem.mp3');   
+    } else if (percentScore < 70) {
         answerResultDiv.text('You have lost the game !');  
-        finalResultDiv.text('You got ' + correctAnswers + ' correct ' + ' out of ' + gameQuestions.length);
+        finalResultDiv.text(`Only ${correctAnswers} out of ${gameQuestions.length}? Time to brush up`); 
         imageUpdate.show().attr('src', 'assets/images/lose.webp'); 
-        gameSound.attr('src', 'assets/audio/lose_game.mp3') 
-    } 
+        gameSound.attr('src', 'assets/audio/lose_game.mp3'); 
+    }   
  }  
     
-// Start button that hides initial Div and shows Div with questions and timer
+// Start button that hides initial Div and shows Div with questions and timer ***** account for resetting the game
 startButton.on('click', function () {
+    indexOfUsed = []; 
+    questionsUsed = 0; 
+    correctAnswers = 0; 
+    wrongAnswers = 0;
+    timeoutAnswers = 0;  
+    timeUsed = 0; 
+    percentScore = 0; 
+    secondsRemaining = 0;
     gameStartDiv.hide();  
+    startButton.hide();   
     gamePlayDiv.show(); 
+    finalMessageDiv.hide(); 
     gameSound.attr('src', 'assets/audio/button_click.mp3')  
     questionGenerator();     
 }); 
-
+ 
  
 });       
 
